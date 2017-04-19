44c5768de930ba56a28d181fa779ba89fac2ea41
Adrielsr <adrielsr3@gmail.com>
2017-04-19 17:06:05 +0200

    Webjars + Estilos

--- src/main/java/es/aromano/WebSecurityConfig.java
+++ src/main/java/es/aromano/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -24,6 +25,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureUrl("/login?error")
                 .and()
             .logout().permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+            .antMatchers("/webjars/**","/resources/**", "/static/**", "/css/**", "/img/**");
     }
 
 
