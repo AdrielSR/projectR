e2b7e450fe8ce2f49bb52285b51f12f5e5c574fc
AdrielSR <adrielsr3@gmail.com>
2017-05-02 19:12:16 +0200

    Mostrando calendario

--- src/main/java/es/aromano/reservas/web/ReservaController.java
+++ src/main/java/es/aromano/reservas/web/ReservaController.java
@@ -1,8 +1,19 @@
 package es.aromano.reservas.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ReservaController {
 
+	
+	@RequestMapping(value = "/reservas", method = RequestMethod.GET)
+	public String reservas(Model model){
+		model.addAttribute("view", "prueba-reservas");
+		
+		return "index";
+	}
+	
 }
