c3fdf7ae4f071cefa6e5e54385f06e1d19410787
Adrielsr <adrielsr3@gmail.com>
2017-05-08 17:14:34 +0200

    Datetimepickers + botón agregar reserva

--- src/main/resources/static/js/reservas-calendario.js
+++ src/main/resources/static/js/reservas-calendario.js
@@ -5,15 +5,41 @@ $(document).ready(function(){
  	reqHeaders[header] = token;
 
 
-    $('#datetimepicker1').datetimepicker({
-		format: 'd/m/Y H:i'
+    $("input[id^='datetimepicker']").datetimepicker({
+		format: 'd/m/Y H:i',
+		lang: 'es',
+		step: 30,
+		mask: true
 	});
 
-    $('#datetimepicker2').datetimepicker({
-        format: 'd/m/Y H:i'
+
+    /// Controla que la hora de fin no sea menor que la de inicio ///
+    $("#datetimepicker1").change(function(){
+        var comienzo = toIso8601($('#datetimepicker1').val());
+        var m = new moment(comienzo);
+        var fin = m.add(1,'hours');
+        $('#datetimepicker2').val(fin.format("DD/MM/YYYY HH:mm"));
     });
 
-	$("#crear-reserva").click(function(){
+    $("#datetimepicker2").change(function(){
+        var comienzo = toIso8601($('#datetimepicker1').val());
+        var fin = toIso8601($('#datetimepicker2').val());
+        var start = new moment(comienzo)
+        var end = new moment(fin);
+        if(end.isBefore(start)){
+            $('#datetimepicker2').val(start.add(1,'hours').format("DD/MM/YYYY HH:mm"));
+        }
+        else{
+            $('#datetimepicker2').val(end.format("DD/MM/YYYY HH:mm"));
+        }
+
+    });
+
+
+
+
+
+    $("#crear-reserva").click(function(){
 		var reserva = {};
 		reserva.title = $("#asunto").val();
 		reserva.start = toIso8601($("#datetimepicker1").val());
