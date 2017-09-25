66bb8c3c4a2093836f8688290d7506587c883f8c
Adrielsr <adrielsr3@gmail.com>
2017-09-25 17:21:02 +0200

    Maquetación seleccionar espacio

---
+++ src/main/resources/static/js/reser-select-espacio.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+
+
+    $(".owl-carousel").owlCarousel({
+
+        margin: 7,
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items: 2
+            },
+            400:{
+                items: 4
+            },
+            600:{
+                items: 6
+            }
+        }
+    });
+
+
+});
