1f96382b2bb1aa9676fbed9bc10cd874aa673368
Adrielsr <adrielsr3@gmail.com>
2017-09-26 10:28:15 +0200

    Maquetación seleccionar espacio

--- src/main/resources/static/js/reser-select-espacio.js
+++ src/main/resources/static/js/reser-select-espacio.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 
 
-
-
     $(".owl-carousel").owlCarousel({
 
         margin: 7,
@@ -21,4 +19,31 @@ $(document).ready(function(){
     });
 
 
+    addItemClickEvent();
+    selectFirstItem();
+
 });
+
+
+function addItemClickEvent(){
+    $('.card').on('click', function(event){
+        var $this = $(this);
+        var idEspacio = $this.find('input').val();
+        // Actualizo la url del boton siguiente
+        var link =  baseURL + 'espacio/' + idEspacio + '/reservas';
+        $("#espacio_link").attr("href",link);
+
+        $(".espacioSeleccionado").removeClass("espacioSeleccionado");
+
+        // Marco el seleccionado
+        $this.find('.card-content').addClass('espacioSeleccionado');
+
+        //LLamada ajax para actualizar los datos
+        //getEspacioById(idEspacio, reqHeaders);
+
+    });
+}
+
+function selectFirstItem(){
+    $(".card-content:first").addClass('espacioSeleccionado');
+}
